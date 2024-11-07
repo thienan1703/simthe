91948c6908357bcf99cfe6f5913fb5bf95a1d6d9
thienan1703 <dangducthienan17032004@gmail.com>
2024-11-07 16:42:42 +0700

    Add files via upload

---
+++ DatabaseConnection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Thesim
+{
+    public class DatabaseConnection
+    {
+        private readonly string connectionString = "Server=DESKTOP-ONB5IAF;Database=Thesim;Integrated Security=True;";
+
+        public DataTable GetSimData()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Sim ORDER BY NgayKichHoat ASC";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+    }
+}
---
+++ Form1.Designer (1).cs
@@ -0,0 +1,77 @@
+﻿namespace Thesim
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewSim = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSim)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewSim
+            // 
+            this.dataGridViewSim.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewSim.Location = new System.Drawing.Point(71, 55);
+            this.dataGridViewSim.Name = "dataGridViewSim";
+            this.dataGridViewSim.Size = new System.Drawing.Size(539, 271);
+            this.dataGridViewSim.TabIndex = 0;
+            this.dataGridViewSim.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewSim_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(288, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thông tin sim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridViewSim);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewSim)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewSim;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
